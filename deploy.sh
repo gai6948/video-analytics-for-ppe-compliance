f86df66f755fa1f4c811462e4fbbf34e3615baad
Gaius Chan <gaiuscyw@amazon.com>
2021-02-28 20:14:06 +0800

    Removed test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 npx cdk deploy --require-approval never --json true -O pipeline-output.json
-codecommiturl=$(jq '.VideoAnalyticsPipelineStack.CodeCommitRepoSshUrl' pipeline-output.json)
+codecommiturl=$(jq -r '.VideoAnalyticsPipelineStack.CodeCommitRepoSshUrl' pipeline-output.json)
 git remote add codecommit $codecommiturl
 # sed command to uncomment stage
 git add .
